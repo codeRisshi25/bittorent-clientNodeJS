ad8b4418da666f46eedf412a649a8036b5d93c8b
Risshi25 <risshirajsen@gmail.com>
2025-03-18 12:15:47 +0530

    added handshake handler

--- src/download.js
+++ src/download.js
@@ -3,22 +3,48 @@
 import net from "net";
 import { Buffer } from "buffer";
 import getPeers from "./tracker.js";
+import * as message from "./message.js";
 
 function main(torrent) {
   getPeers(torrent, (peers) => {
-    peers.forEach(download);
+    peers.forEach((peer) => download(peer, torrent));
   });
 }
 
-function download(peer) {
+function download(peer, torrent) {
   const socket = new net.Socket();
   socket.on("error", console.log);
   socket.connect(peer.port, peer.ip, function () {
-    socket.write(Buffer.from("hello world"));
+    socket.write(message.buildHandshake(torrent));
   });
+  onWholeMsg(socket, (msg) => msgHandler(msg, socket));
+}
 
-  socket.on("data", (responseBuffer) => {
-    // do something with response Buffer
+function msgHandler(msg, socket) {
+  if (isHandshake(msg)) socket.write(message.buildInterested());
+}
+
+function isHandshake(msg) {
+  return (
+    msg.length === msg.readUInt8(0) + 49 &&
+    msg.toString("utf8", 1) === "BitTorrent protocol"
+  );
+}
+
+function onWholeMsg(socket, callback) {
+  let savedBuf = Buffer.alloc(0);
+  let handshake = true;
+
+  socket.on("data", (recvBuf) => {
+    const msgLen = () =>
+      handshake ? savedBuf.readUInt8(0) + 49 : savedBuf.readInt32BE(0) + 4;
+    savedBuf = Buffer.concat([savedBuf, recvBuf]);
+
+    while (savedBuf.length >= 4 && savedBuf.length >= msgLen()) {
+      callback(savedBuf.slice(0, msgLen()));
+      savedBuf = savedBuf.slice(msgLen());
+      handshake = false;
+    }
   });
 }
 
